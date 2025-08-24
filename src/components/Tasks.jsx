501431210748780328e0c072343a00259852c41d
Luìz Moraes <luizmoraesim@gmail.com>
2025-08-24 17:57:00 -0300

    feat: add initial Task, Button components and icon files

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,29 @@
-import { useState } from "react"
+import Button from "./Button"
+import { AddIcon, TrashIcon } from "../assets/icons/index"
 
-function Tasks() {}
+function Tasks() {
+  return (
+    <div className="w-full px-8 py-16">
+      <div className="w-fullß flex justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00ADB5]">
+            Minhas Tarefas
+          </span>
+          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant="ghost">
+            Limpar tarefas
+            <TrashIcon />
+          </Button>
+          <Button variant="primary">
+            Nova tarefa
+            <AddIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Tasks
