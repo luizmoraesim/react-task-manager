f93fd92cd2aa2e6cd6d2f19e0db1866be766e31b
Luìz Moraes <luizmoraesim@gmail.com>
2025-09-03 18:15:01 -0300

    chore: add PropTypes to Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,7 @@
 import { forwardRef } from "react"
 import InputLabel from "./InputLabel"
 import InputErrorMessage from "./InputErrorMessage"
+import PropTypes from "prop-types"
 
 const Input = forwardRef(({ label, error, ...rest }, ref) => {
   return (
@@ -17,5 +18,12 @@ const Input = forwardRef(({ label, error, ...rest }, ref) => {
 })
 
 Input.displayName = "Input"
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
+  placeholder: PropTypes.string,
+  id: PropTypes.string.isRequired,
+}
 
 export default Input
