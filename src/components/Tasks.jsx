11347eab1637f2bbe051afa8598f3ab1cd9c788f
Luìz Moraes <luizmoraesim@gmail.com>
2025-09-04 00:38:47 -0300

    feat: ensure React create a new task on API

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -63,7 +63,14 @@ function Tasks() {
     toast.success("Tarefa deletada com sucesso")
   }
 
-  const handleAddTaskSubmit = (task) => {
+  const handleAddTaskSubmit = async (task) => {
+    const response = await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      body: JSON.stringify(task),
+    })
+    if (!response.ok) {
+      return toast.error("Erro ao adicionar tarefa. Tente novamente mais tarde")
+    }
     setTasks([...tasks, task])
     toast.success("Tarefa adicionada com sucesso!")
   }
